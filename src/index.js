fbbd8f4e60856335e815acb4a0b53a1381fb2334
HelenKolesnikovich <solnechnaya_88@list.ru>
2021-02-26 23:14:47 +0300

    feat: add solution for 'Typical-Arrays-Problems'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array === undefined || array.length == 0)
+	{
+		return 0;
+	}
+	let minValue = array[0];
+	array.forEach(function(element){
+	if(element < minValue){
+		minValue = element;
+	}
+	});
+	return minValue;
+	
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length == 0){
+		return 0;
+	}	
+
+	let maxValue = array[0];
+	array.forEach(function(element){
+		if(element > maxValue)
+		{
+			maxValue = element;
+		}
+	});
+	return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length == 0)
+	{
+		return 0;
+	}
+
+	let sumValues = 0;
+	array.forEach(function(element){
+		sumValues += element;
+	});
+	return sumValues / array.length;
 }
